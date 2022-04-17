0a3a5cdd4ea54c73a3ab3a1c1acf48bf9c996070
Wanderley <davidgomeswanderley@gmail.com>
2022-04-17 15:54:48 -0300

    componente layout

--- src/pages/estiloso.jsx
+++ src/pages/estiloso.jsx
@@ -1,9 +1,13 @@
 import styles from '../styles/Estiloso.module.css'
-import Link from 'next/dist/client/link'
+import Layout from '../components/Layout'
 
 export default function Estiloso(){
-    return (<div className={styles.roxo}>
+    return (
+        <Layout titulo='Exemplo de css modularizado'> 
+            <div className={styles.roxo}>
                 <h1>Estilo usando CSS Modulo</h1>
-                <Link href="/">Home</Link>
-            </div>)
+            </div>
+        </Layout>
+       
+    )
 }
